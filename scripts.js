6d0ae8fb687b4bb798938ac5cfb80174bf8f6212
Sumit Yadav <sumit.yadav@qs.com>
2026-09-02 16:11:03 +0530

    FS V1.15 Commented

--- scripts.js
+++ scripts.js
@@ -68,7 +68,7 @@ import { functionFilterScroll } from './modules/finsweet/filter-scroll.js';
 
 
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("## [Filter Scroll V1.15] ##"); // ADD THIS LINE
+  console.log("## [Filter Scroll V1.15 | Commented] ##"); // ADD THIS LINE
 
   gsap.registerPlugin(
     ScrollTrigger,
@@ -233,7 +233,7 @@ document.addEventListener("DOMContentLoaded", function () {
   initCountdownFooterOffset();
   functionCaseStudyBento();
   initHubspotPopup();
-  functionFilterScroll();
+  // functionFilterScroll();
 
   }
 });
